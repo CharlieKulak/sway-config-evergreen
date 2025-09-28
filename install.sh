7275fcd49b81f121cb2232c699935ab1229de3b1
Charlie Kulak <121259171+CharlieKulak@users.noreply.github.com>
2025-09-27 20:27:33 -0700

    Added a install script :D

---
+++ install.sh
@@ -0,0 +1,9 @@
+chmod +x autotiling
+chmod +x wallpaper
+
+mv autotiling /usr/bin
+mv wallpaper /usr/bin
+echo installing flatpak apps...
+flatpak install im.riot.Riot
+flatpak install dev.vencord.Vesktop
+echo finished! yeah wasn't that big of a install script ngl
